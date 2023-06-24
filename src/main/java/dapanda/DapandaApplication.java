2a7328cd332b980d5f91ac8df9024672aad3efa8
taewoongjung <taewoongjung@taewoongjungui-MacBookPro.local>
2023-06-24 15:06:56 +0900

    first commit

---
+++ src/main/java/dapanda/DapandaApplication.java
@@ -0,0 +1,13 @@
+package dapanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DapandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DapandaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dapanda/DapandaApplicationTests.java
@@ -0,0 +1,13 @@
+package dapanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DapandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
