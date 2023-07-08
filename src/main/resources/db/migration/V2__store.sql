a9ce7bdba4fa266cda2430bbd51e8b59eb41b669
taewoongjung <aipooh8882@naver.com>
2023-07-09 02:46:35 +0900

    issue-#2: store 테이블 추가

---
+++ src/main/resources/db/migration/V2__store.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `store`
+(
+    `id`            BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `store_name`    VARCHAR(20)         NOT NULL                  COMMENT '가게 이름',
+    `category`      VARCHAR(20)         NOT NULL                  COMMENT '가게가 속한 카테고리',
+    `ceo_id`        BIGINT(20) UNSIGNED NOT NULL                  COMMENT '가게 사장님 id',
+    `created_at`    DATETIME(6)         NOT NULL   DEFAULT CURRENT_TIMESTAMP(6),
+    `last_modified` DATETIME(6)         NOT NULL   DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (ceo_id) REFERENCES customer (id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4 COMMENT '가게 정보';
