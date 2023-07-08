5b9ae02a16dd83812719a1ea5cfa442e76f89492
taewoongjung <aipooh8882@naver.com>
2023-07-09 02:46:20 +0900

    issue-#2: customer 테이블 수정

--- src/main/resources/db/migration/V1__customer.sql
+++ src/main/resources/db/migration/V1__customer.sql
@@ -6,7 +6,7 @@ CREATE TABLE `customer`
     `password`      VARCHAR(100)        NOT NULL                  COMMENT '고객 비밀번호',
     `tel`           VARCHAR(20)         NOT NULL                  COMMENT '고객 전화번호',
     `created_at`    DATETIME(6)         NOT NULL   DEFAULT CURRENT_TIMESTAMP(6),
-    `updated_at`    DATETIME(6)         NOT NULL   DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+    `last_modified` DATETIME(6)         NOT NULL   DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4 COMMENT '고객 정보';
