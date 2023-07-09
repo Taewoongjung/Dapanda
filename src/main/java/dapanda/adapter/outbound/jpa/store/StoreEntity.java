b463d79e7a1de77c80b63a9c399c6a08be18c42b
taewoongjung <aipooh8882@naver.com>
2023-07-10 01:36:44 +0900

    issue-#2: StoreEntity 클래스 추가

---
+++ src/main/java/dapanda/adapter/outbound/jpa/store/StoreEntity.java
@@ -0,0 +1,59 @@
+package dapanda.adapter.outbound.jpa.store;
+
+import dapanda.adapter.outbound.jpa.BaseEntity;
+import dapanda.adapter.outbound.jpa.customer.CustomerEntity;
+import dapanda.domain.store.Store;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@Table(name = "store")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class StoreEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CustomerEntity> customers = new ArrayList<>();
+
+    private String storeName;
+
+    private String category;
+
+    private StoreEntity(
+            final long id,
+            final String storeName,
+            final String category
+    ) {
+        super(LocalDateTime.now(), LocalDateTime.now());
+
+        this.id = id;
+        this.storeName = storeName;
+        this.category = category;
+    }
+
+    public static StoreEntity of(
+            final long id,
+            final String storeName,
+            final String category
+    ) {
+        return new StoreEntity(id, storeName, category);
+    }
+
+//    public static Store toStorePojo(final StoreEntity entity) {
+//        return Store.of(
+//                entity.getId(),
+//                entity.storeName,
+//                entity.category
+//        )
+//    }
+}
