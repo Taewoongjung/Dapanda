681b6dca6955f7ce6ee43e9259a67ec24bc143e4
taewoongjung <aipooh8882@naver.com>
2023-07-10 01:35:10 +0900

    issue-#2: Product 클래스 리팩토링

--- src/main/java/dapanda/domain/store/product/Product.java
+++ src/main/java/dapanda/domain/store/product/Product.java
@@ -1,55 +1,39 @@
 package dapanda.domain.store.product;
 
+import dapanda.domain.store.Store;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 import java.time.LocalDateTime;
 
-import static dapanda.adapter.common.ErrorType.INVALID_PRODUCT_NAME;
-import static dapanda.adapter.util.ObjectCheck.require;
-
 @Getter
 @ToString
 @RequiredArgsConstructor
-public class Product {
+public abstract class Product {
     private long id;
     private String name;
     private String brandName;
-    private long price;
-    private boolean isUse;
+    private int amount;
+    private Store store;
     private LocalDateTime createdAt;
     private LocalDateTime lastModified;
 
-    private Product(
+    public Product(
             final long id,
             final String name,
             final String brandName,
-            final long price,
-            final boolean isUse,
+            final int amount,
+            final Store store,
             final LocalDateTime createdAt,
             final LocalDateTime lastModified
     ) {
         this.id = id;
         this.name = name;
         this.brandName = brandName;
-        this.price = price;
-        this.isUse = isUse;
+        this.amount = amount;
+        this.store = store;
         this.createdAt = createdAt;
         this.lastModified = lastModified;
-    }
-
-    public static Product of(
-            final long id,
-            final String name,
-            final String brandName,
-            final long price,
-            final boolean isUse,
-            final LocalDateTime createdAt,
-            final LocalDateTime lastModified
-    ) {
-        require(o -> name == null, name, INVALID_PRODUCT_NAME);
-
-        return new Product(id, name, brandName, price, isUse, createdAt, lastModified);
     }
 }
