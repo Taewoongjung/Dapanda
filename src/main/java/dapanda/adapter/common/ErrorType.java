47d88322086b860d9c1a79099ce0209ffc450fe2
taewoongjung <aipooh8882@naver.com>
2023-07-09 12:28:44 +0900

    issue-#2: Customer 클래스 추가

--- src/main/java/dapanda/adapter/common/ErrorType.java
+++ src/main/java/dapanda/adapter/common/ErrorType.java
@@ -9,7 +9,11 @@ public enum ErrorType {
 
     INVALID_CUSTOMER_NAME(1001, "이름은 필수값 입니다."),
     INVALID_CUSTOMER_EMAIL(1002, "이메일은 필수값 입니다."),
-    INVALID_CUSTOMER_TEL(1003, "전화번호는 필수값 입니다.")
+    INVALID_CUSTOMER_TEL(1003, "전화번호는 필수값 입니다."),
+
+    INVALID_STORE_NAME(2001, "가게이름은 필수값 입니다."),
+    INVALID_STORE_CATEGORY(2002, "가게 카테고리는 필수값 입니다."),
+    INVALID_STORE_CEO_ID(2003, "가게 사장님 ID는 필수값 입니다."),
     ;
 
     private final int code;
---
+++ src/main/java/dapanda/domain/store/Store.java
@@ -0,0 +1,55 @@
+package dapanda.domain.store;
+
+import dapanda.domain.customer.Customer;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+import static dapanda.adapter.common.ErrorType.*;
+import static dapanda.adapter.util.ObjectCheck.require;
+
+@Getter
+@ToString
+@RequiredArgsConstructor
+public class Store {
+
+    private long id;
+    private String storeName;
+    private String category;
+    private Customer ceoId;
+    private LocalDateTime createdAt;
+    private LocalDateTime lastModified;
+
+    private Store(
+            final long id,
+            final String storeName,
+            final String category,
+            final Customer ceoId,
+            final LocalDateTime createdAt,
+            final LocalDateTime lastModified
+    ) {
+        this.id = id;
+        this.storeName = storeName;
+        this.category = category;
+        this.ceoId = ceoId;
+        this.createdAt = createdAt;
+        this.lastModified = lastModified;
+    }
+
+    public static Store of(
+            final long id,
+            final String storeName,
+            final String category,
+            final Customer ceoId,
+            final LocalDateTime createdAt,
+            final LocalDateTime lastModified
+    ) {
+        require(o -> storeName == null, storeName, INVALID_STORE_NAME);
+        require(o -> category == null, category, INVALID_STORE_CATEGORY);
+        require(o -> ceoId == null, ceoId, INVALID_STORE_CEO_ID);
+
+        return Store.of(id, storeName, category, ceoId, createdAt, lastModified);
+    }
+}
