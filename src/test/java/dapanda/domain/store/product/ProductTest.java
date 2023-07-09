1305516def5092678b608c530ceaa72413a23777
taewoongjung <aipooh8882@naver.com>
2023-07-10 01:35:39 +0900

    issue-#2: Product 테스트 베이스 코드 추가

---
+++ src/test/java/dapanda/domain/store/product/ProductTest.java
@@ -0,0 +1,16 @@
+package dapanda.domain.store.product;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Product를")
+class ProductTest {
+
+//    @Test
+//    @DisplayName("생성할 수 있다.")
+//    void test1() {
+//        assertDoesNotThrow(() -> );
+//    }
+}
---
+++ src/testFixtures/java/product/ProductFixture.java
@@ -0,0 +1,11 @@
+package product;
+
+import dapanda.domain.store.product.Product;
+
+public class ProductFixture {
+
+//    public static Product PRODUCT = Product.of(
+//            1L,
+//            ""
+//    )
+}
