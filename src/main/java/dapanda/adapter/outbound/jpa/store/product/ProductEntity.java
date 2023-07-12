fcb7da37edc73e7b5c900870f07ff97dc9cfceb4
taewoongjung <aipooh8882@naver.com>
2023-07-12 22:12:21 +0900

    issue-#2: 리팩토링

--- src/main/java/dapanda/adapter/outbound/jpa/store/product/ProductEntity.java
+++ src/main/java/dapanda/adapter/outbound/jpa/store/product/ProductEntity.java
@@ -11,8 +11,8 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @Getter
-@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class ProductEntity extends BaseEntity {
 
