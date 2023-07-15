1d9b29e5fae60ca23d74535356d0d3b64a408992
taewoongjung <aipooh8882@naver.com>
2023-07-15 17:30:31 +0900

    issue-#3: 리팩토링

--- src/main/java/dapanda/adapter/outbound/jpa/store/product/repository/ProductJpaRepository.java
+++ src/main/java/dapanda/adapter/outbound/jpa/store/product/repository/ProductJpaRepository.java
@@ -1,5 +1,6 @@
-package dapanda.adapter.outbound.jpa.store.product;
+package dapanda.adapter.outbound.jpa.store.product.repository;
 
+import dapanda.adapter.outbound.jpa.store.product.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductJpaRepository extends JpaRepository<ProductEntity, Long> {
--- src/main/java/dapanda/adapter/outbound/jpa/store/repository/StoreJpaRepository.java
+++ src/main/java/dapanda/adapter/outbound/jpa/store/repository/StoreJpaRepository.java
@@ -1,5 +1,6 @@
-package dapanda.adapter.outbound.jpa.store;
+package dapanda.adapter.outbound.jpa.store.repository;
 
+import dapanda.adapter.outbound.jpa.store.StoreEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StoreJpaRepository extends JpaRepository<StoreEntity, Long> {
