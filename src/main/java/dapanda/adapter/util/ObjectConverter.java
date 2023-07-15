bf1005a895eff7c934928886a3c854911b301ef4
taewoongjung <aipooh8882@naver.com>
2023-07-15 17:31:02 +0900

    issue-#3: Product pojo converter 추가

--- src/main/java/dapanda/adapter/util/ObjectConverter.java
+++ src/main/java/dapanda/adapter/util/ObjectConverter.java
@@ -2,10 +2,17 @@ package dapanda.adapter.util;
 
 import dapanda.adapter.outbound.jpa.customer.CustomerEntity;
 import dapanda.adapter.outbound.jpa.store.StoreEntity;
+import dapanda.adapter.outbound.jpa.store.product.ProductEntity;
+import dapanda.adapter.outbound.jpa.store.product.cloth.ClothEntity;
 import dapanda.adapter.outbound.jpa.store.product.food.FoodEntity;
 import dapanda.domain.customer.Customer;
 import dapanda.domain.store.Store;
+import dapanda.domain.store.product.Product;
+import dapanda.domain.store.product.cloth.Cloth;
 import dapanda.domain.store.product.food.Food;
+
+import static dapanda.domain.store.StoreCategoryType.CLOTH;
+import static dapanda.domain.store.StoreCategoryType.FOOD;
 
 public class ObjectConverter {
 
@@ -30,8 +37,19 @@ public class ObjectConverter {
         );
     }
 
+    @SuppressWarnings("unchecked")
+    public static <T extends Product> T toProductPojo(ProductEntity entity) {
+        if (CLOTH.equals(entity.getStore().getCategory())) {
+            return (T) toClothPojo((ClothEntity) entity);
 
-    public static Food toFoodPojo(final FoodEntity entity) {
+        } else if (FOOD.equals(entity.getStore().getCategory())) {
+            return (T) toFoodPojo((FoodEntity) entity);
+
+        } else {
+            throw new IllegalArgumentException("Unsupported entity type: " + entity.getClass());
+        }
+    }
+    public static Product toFoodPojo(final FoodEntity entity) {
         return Food.of(
                 entity.getId(),
                 entity.getName(),
@@ -45,7 +63,7 @@ public class ObjectConverter {
                 entity.getLastModified()
         );
     }
-    public FoodEntity toFoodEntity(final Food pojo) {
+    public static ProductEntity toFoodEntity(final Food pojo) {
         return FoodEntity.of(
                 pojo.getId(),
                 pojo.getName(),
@@ -56,6 +74,33 @@ public class ObjectConverter {
                 pojo.getDescription()
         );
     }
+
+    public static Product toClothPojo(final ClothEntity entity) {
+        return Food.of(
+                entity.getId(),
+                entity.getName(),
+                entity.getBrandName(),
+                entity.getAmount(),
+                toStorePojo(entity.getStore()),
+                entity.getPrice(),
+                entity.isUse(),
+                entity.getDescription(),
+                entity.getCreatedAt(),
+                entity.getLastModified()
+        );
+    }
+    public static ProductEntity toClothEntity(final Cloth pojo) {
+        return ClothEntity.of(
+                pojo.getId(),
+                pojo.getName(),
+                pojo.getBrandName(),
+                pojo.getAmount(),
+                pojo.getPrice(),
+                pojo.isUse(),
+                pojo.getDescription()
+        );
+    }
+
 
     public static StoreEntity toStoreEntity(final Store pojo) {
         return StoreEntity.of(
