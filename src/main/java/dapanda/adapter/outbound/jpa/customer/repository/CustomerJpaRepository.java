fb570208751ebbd80575c73c1fe70dd71c5bfbba
taewoongjung <aipooh8882@naver.com>
2023-07-15 17:29:49 +0900

    issue-#3: 리팩토링

--- src/main/java/dapanda/adapter/outbound/jpa/customer/repository/CustomerJpaRepository.java
+++ src/main/java/dapanda/adapter/outbound/jpa/customer/repository/CustomerJpaRepository.java
@@ -1,5 +1,6 @@
-package dapanda.adapter.outbound.jpa.customer;
+package dapanda.adapter.outbound.jpa.customer.repository;
 
+import dapanda.adapter.outbound.jpa.customer.CustomerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerJpaRepository extends JpaRepository<CustomerEntity, Long> {
