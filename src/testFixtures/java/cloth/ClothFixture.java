a28b3ebe06f6348152a44ffec76093dbf6ba2ed7
taewoongjung <aipooh8882@naver.com>
2023-07-16 02:34:34 +0900

    issue-#3: Cloth 수정에 따른 리팩토링

--- src/testFixtures/java/cloth/ClothFixture.java
+++ src/testFixtures/java/cloth/ClothFixture.java
@@ -10,7 +10,7 @@ public class ClothFixture {
 
     public static Cloth CLOTH_NEW = Cloth.of(
             1, "멋진바지", "멋진브랜드",
-            12, STORE, 70000, true,
+            12, 70000, true,
             "정말멋져요", LocalDateTime.now(), LocalDateTime.now()
     );
 }
