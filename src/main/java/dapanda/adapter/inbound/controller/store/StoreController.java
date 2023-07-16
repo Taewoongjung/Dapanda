9ff5440f6b64b204fe7ea2589e78898a09787918
taewoongjung <aipooh8882@naver.com>
2023-07-16 22:28:26 +0900

    issue-#3-1: order, get 컨트롤러 추가

--- src/main/java/dapanda/adapter/inbound/controller/store/StoreController.java
+++ src/main/java/dapanda/adapter/inbound/controller/store/StoreController.java
@@ -1,17 +1,42 @@
 package dapanda.adapter.inbound.controller.store;
 
 import dapanda.adapter.inbound.controller.request.WebRequest;
+import dapanda.application.store.StoreService;
+import dapanda.application.store.dto.StoreServiceDto;
+import dapanda.domain.order.Order;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
+@RequiredArgsConstructor
 public class StoreController {
 
+    private final StoreService storeService;
+
     @PostMapping(value = "/stores/{storeId}/products/{productId}")
-    public void orderProduct(
+    public void order(
             @PathVariable final long storeId,
             @PathVariable final long productId,
             @RequestBody final WebRequest.OrderProductWebRequestV1 webRequestV1
     ) {
+        log.info("{}", webRequestV1);
 
+        storeService.order(new StoreServiceDto.OrderDto(
+                storeId,
+                productId,
+                webRequestV1.orderAmount()
+        ));
+    }
+
+    @GetMapping(value = "/stores/{storeId}/orders/{orderId}")
+    public Order getOrder(
+            @PathVariable final long storeId,
+            @PathVariable final long orderId
+    ) {
+        log.info("{} {}", storeId, orderId);
+
+        return storeService.findOrder(storeId, orderId);
     }
 }
